db2bb0b3eb339740288cb1bb1a184b0dadbb8cf5
Calliope Epstein-Pawlak <kiki360@rootlike.com>
2025-04-11 13:16:49 -0500

    I made the variable number2 into an optional

--- ContentView.swift
+++ ContentView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State var number1: Int = 0
-    @State var number2: Int = 0
+    @State var number2: Int?
     var body: some View {
         
        Text("Hello")
