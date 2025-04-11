0672ccb028833ddd0627dc4c534845ec63147350
Jayden Kim <jkim8208@stu.d214.org>
2025-04-11 13:12:47 -0500

    I added a textfield bonded to the variable number1

--- ContentView.swift
+++ ContentView.swift
@@ -1,11 +1,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var number1: String = ""
-    @State var number2 = 0
+    @State var number1: Int = 0
+    @State var number2: Int = 0
     var body: some View {
         
        Text("Hello")
         TextField("Number 2", value: $number2, format: .number)
+        
+        TextField("Enter Number 1", value: $number1, format: .number)
+        
     }
 }
