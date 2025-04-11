0b69ccbb38a7915198bc4d14976ae468e88906cd
Jayden Kim <jkim8208@stu.d214.org>
2025-04-11 13:02:33 -0500

    I created an @State variable number1

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var number1: String = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        
+       Text("Hello")
     }
 }
