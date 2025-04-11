36c02fe94babd0e256967b7d8700cf5854df49b4
Jayden Kim <jkim8208@stu.d214.org>
2025-04-11 13:16:14 -0500

    I created an answer variable and I created a text that converts answer into a string and displays it.

--- ContentView.swift
+++ ContentView.swift
@@ -3,12 +3,15 @@ import SwiftUI
 struct ContentView: View {
     @State var number1: Int = 0
     @State var number2: Int = 0
+    @State var answer: Int = 0
     var body: some View {
         
        Text("Hello")
         TextField("Number 2", value: $number2, format: .number)
         
         TextField("Enter Number 1", value: $number1, format: .number)
+        
+        Text(String(answer))
         
     }
 }
