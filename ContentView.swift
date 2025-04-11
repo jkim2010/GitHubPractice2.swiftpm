6ee50ad2c1bd5abb023756b3cfd5c8469a38b1bb
Calliope Epstein-Pawlak <kiki360@rootlike.com>
2025-04-11 13:14:56 -0500

    I added modifiers to the textfield linked to the variable number2, I added a button with no action and a label saying "calculate," and I made the variable number2 an optional

--- ContentView.swift
+++ ContentView.swift
@@ -2,10 +2,26 @@ import SwiftUI
 
 struct ContentView: View {
     @State var number1: String = ""
-    @State var number2 = 0
+    @State var number2: Int?
     var body: some View {
         
        Text("Hello")
         TextField("Number 2", value: $number2, format: .number)
+            .padding()
+            .multilineTextAlignment(.center)
+            .textFieldStyle(.roundedBorder)
+        
+        Button {
+            
+        } label: {
+            RoundedRectangle(cornerRadius: 25.5)
+                .frame(width: 150, height: 75)
+                .overlay {
+                    Text("Calculate")
+                        .foregroundStyle(.black)
+                }
+                .shadow(radius: 7)
+        }
+
     }
 }
